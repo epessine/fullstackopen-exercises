bf70475cc301686d42c1e2715fc709e0b2f05142
Eduardo Pessine <eduardopessine@gmail.com>
2021-03-19 13:06:17 -0300

    Exercises 1.1.-1.2

---
+++ part1/src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.item.part} {props.item.exercise}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part item={props.content[0]} />
+      <Part item={props.content[1]} />
+      <Part item={props.content[2]} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  var total = 0;
+  props.content.forEach(part => {
+    total += part.exercise
+  });
+
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const content = [
+    {
+      part: 'Fundamentals of React',
+      exercise: 10
+    },
+    {
+      part: 'Using props to pass data',
+      exercise: 7
+    },
+    {
+      part: 'State of a component',
+      exercise: 14
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={content} />
+      <Total content={content} />
+    </div>
+  );
+};
+
+export default App;
