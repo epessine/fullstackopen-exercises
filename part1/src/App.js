973d040636236d4787dc9c176f21d2359e782fa4
Eduardo Pessine <eduardopessine@gmail.com>
2021-03-21 14:16:28 -0300

    Exercises 1.3.-1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.content.name}</h1>
     </div>
   );
 };
@@ -12,7 +12,7 @@ const Part = (props) => {
   return (
     <div>
       <p>
-        {props.item.part} {props.item.exercise}
+        {props.part.name} {props.part.exercise}
       </p>
     </div>
   );
@@ -21,16 +21,16 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part item={props.content[0]} />
-      <Part item={props.content[1]} />
-      <Part item={props.content[2]} />
+      <Part part={props.content.parts[0]} />
+      <Part part={props.content.parts[1]} />
+      <Part part={props.content.parts[2]} />
     </div>
   );
 };
 
 const Total = (props) => {
   let total = 0;
-  props.content.forEach(part => {
+  props.content.parts.forEach(part => {
     total += part.exercise
   });
 
@@ -42,27 +42,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const content = [
-    {
-      part: 'Fundamentals of React',
-      exercise: 10
-    },
-    {
-      part: 'Using props to pass data',
-      exercise: 7
-    },
-    {
-      part: 'State of a component',
-      exercise: 14
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercise: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercise: 7
+      },
+      {
+        name: 'State of a component',
+        exercise: 14
+      },
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content content={content} />
-      <Total content={content} />
+      <Header content={course} />
+      <Content content={course} />
+      <Total content={course} />
     </div>
   );
 };
