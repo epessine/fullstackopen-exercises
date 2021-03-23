b6f4388101bc9b6492a04e69ad3615aa3abb9303
Eduardo Pessine <eduardopessine@gmail.com>
2021-03-22 23:20:23 -0300

    Exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [bad, setBad] = useState(0);
   const total = (good + neutral + bad);
   const average = '';
-  const percent = (good / total) * 100;
+  const percent = ((good / total) * 100).toFixed(1);
   const title = "give feedback";
 
   return (
@@ -15,7 +15,7 @@ const App = () => {
       <Button text={"good"} handleClick={() => setGood(good + 1)} />
       <Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
       <Button text={"bad"} handleClick={() => setBad(bad + 1)} />
-      <Statistics 
+      <Statistics
         good={good}
         neutral={neutral}
         bad={bad}
@@ -56,19 +56,26 @@ const Statistics = ({ good, neutral, bad, total, average, percent }) => {
   return (
     <div>
       <h1>statistics</h1>
-      <Statistic name={'good'} count={good} />
-      <Statistic name={'neutral'} count={neutral} />
-      <Statistic name={'bad'} count={bad} />
-      <Statistic name={'total'} count={total} />
-      <Statistic name={'average'} count={average} />
-      <Statistic name={'percent'} count={percent} />
+      <table>
+        <tbody>
+          <Statistic name={'good'} count={good} />
+          <Statistic name={'neutral'} count={neutral} />
+          <Statistic name={'bad'} count={bad} />
+          <Statistic name={'total'} count={total} />
+          <Statistic name={'average'} count={average} />
+          <Statistic name={'percent'} count={percent} />
+        </tbody>
+      </table>
     </div>
   )
 }
 
 const Statistic = ({ name, count }) => {
   return (
-    <p>{ name } { count }</p>
+    <tr>
+      <td>{name}</td>
+      <td>{count}</td>
+    </tr>
   )
 }
 
