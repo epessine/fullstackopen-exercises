aed2b98d89499f505450a86805e72d109c7c0551
Eduardo Pessine <eduardopessine@gmail.com>
2021-03-31 00:04:12 -0300

    Exercise 2.12

---
+++ part1-2/countries/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [ countries, setCountries ] = useState([]);
+  const [ filter, setFilter ] = useState([]);
+  let results = [];
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, []);
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value.toLowerCase())
+  }
+
+  const filteredCountries = filter
+    ? countries.filter(country => country.name.toLowerCase().indexOf(filter) !== -1)
+    : countries;
+  
+  if (filteredCountries.length > 10) {
+    results = ['Too many matches, specify another filter'];
+  } else if (filteredCountries.length > 1) {
+    results = filteredCountries.map(country => country.name);
+  } else if (filteredCountries.length === 1) {
+    results = filteredCountries;
+  }
+
+  return (
+    <div>
+      <div>
+        find countries <input value={filter} onChange={handleFilterChange}/>
+      </div>
+      <div>
+        {filteredCountries.length > 1
+          ?
+          results.map(result =>
+            <p>{result}</p>
+          )
+          :
+          results.map(result =>
+            <div>
+              <h1>{result.name}</h1>
+              <p>capital {result.capital}</p>
+              <p>population {result.population}</p>
+              <h2>languages</h2>
+              <ul>
+                {result.languages.map(language =>
+                  <li key={language.id}>{language.name}</li>
+                )}
+              </ul>
+              <img src={result.flag} width="20%" alt="flag"/>
+            </div>
+          )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
