e9e76a9bcbef6114f15b0cf3e0506387b48825d5
Eduardo Pessine <eduardopessine@gmail.com>
2021-03-30 22:45:23 -0300

    Exercise 2.11

--- part1-2/phonebook/src/components/App.js
+++ part1-2/phonebook/src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Search from './Search';
 import List from './List';
 import Form from './Form';
@@ -13,6 +14,14 @@ const App = () => {
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ newSearch, setNewSearch ] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, []);
 
   const handleNameChange = e => {
     setNewName(e.target.value);
